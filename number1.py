1f4629093620cf5e282b3fa9bfa664af645c2ec9
Muhammad Abduh <muhammadabduh@pptp-212-201-75-7.pptp.stw-bonn.de>
2014-10-29 17:19:34 +0100

    it's already finished

--- number1.py
+++ number1.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
+# get rmin and rmax from user
+while True :
+    rmin=raw_input('Input Rmin : ');
     rmax=raw_input('Input Rmax : ');
+    # cast to integer
+    rmin = int(rmin)
+    rmax = int(rmax)
+    if(rmin > rmax):
+        print "Rmin should be less than Rmax\n"
+    else:
+        break
+
 with open('bauckhage.pgm','r') as f:
+# Parsing file pgm
     # marker
     read_var = f.read(3)
     # image width
@@ -29,10 +41,32 @@ with open('bauckhage.pgm','r') as f:
         except :
             break
 
-    # process
-    array_new = []
-    rmin = 30
-    rmax = 35
-    if
-    #Pij = P(i*jmax +j) = Pk
-    print array[2]
+# Process
+# initialize new array
+array_new = []
+for x in range(width*height) :
+    array_new.append(x)
+
+# initialize variable
+i = 0
+j = 0
+h_half = height/2
+w_half = width/2
+
+for j in range(height) :
+    for i in range(width) :
+        if pow(i - w_half,2) + pow(j - h_half,2) >= rmin*rmin:
+            if pow(i - w_half,2) + pow(j - h_half,2) <= rmax*rmax :
+                array_new[i+j*width] = 0
+            else :
+                array_new[i+j*width] = array[i+j*width]
+        else :
+            array_new[i+j*width] = array[i+j*width]
+
+# create new file
+with open('bauckhage_new.pgm','w') as f:
+    f.write("P5\n")
+    f.write("%d %d\n" % (width, height))
+    f.write("255\n")
+    for item in array_new:
+      f.write(chr(item))
