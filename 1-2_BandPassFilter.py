0f4acb2d856d957343850ef12848eae9f8a6910c
Maureen Tanuadji <maureen.tanuadji@dfki.de>
2014-11-15 03:04:46 +0100

    modify the code for ifft process

--- 1-2_BandPassFilter.py
+++ 1-2_BandPassFilter.py
@@ -1,10 +1,11 @@
 import Image
+from matplotlib import pyplot as plt
 import numpy as np
 
 def get_img():
     #Get the input file from user
     while True :
-        filename=raw_input('Input filename of the image : ');
+        filename=raw_input('Input filename of the image : ')
         try:
             with open(filename) as file:
              output = Image.open(filename)
@@ -16,8 +17,8 @@ def get_img():
 def get_r_value():
     #Get rmin and rmax values form user
     while True :
-        rmin=raw_input('Input Rmin : ');
-        rmax=raw_input('Input Rmax : ');
+        rmin=raw_input('Input Rmin : ')
+        rmax=raw_input('Input Rmax : ')
         # cast to integer
         rmin = int(rmin)
         rmax = int(rmax)
@@ -41,10 +42,13 @@ def fft_process(img):
     img_fourier = np.fft.fft2(img)
 
     #Fourier shift
-    img_fshift = np.fft.fftshift(img_fourier)
+    output = np.fft.fftshift(img_fourier)
 
+    return output
+
+def log_of_fft(img_fshift):
     #Take the log of the fourier shift
-    spectrum = np.log(np.abs(img_fshift))
+    spectrum = np.log1p(np.abs(img_fshift))
 
     log_min = spectrum.min()
     log_max = spectrum.max()
@@ -54,65 +58,68 @@ def fft_process(img):
 
     #Make the array from 0 - 255 and cast to integer instead of float
     output = (spectrum * 255).astype(np.uint8)
+
     return output
 
-def suppress_freq(img, ftt_img, rmin, rmax):
+def suppress_freq(img_fshift, rmin, rmax):
     #Initializing variables and array
-    width, height = img.size
+    width, height = np.shape(img_fshift)
 
-    output = np.zeros(shape=(width,height), dtype=np.int)
+    output = img_fshift
 
     i = 0
     j = 0
     h_half = height/2
     w_half = width/2
 
+
     for i in range(height):
         for j in range(width):
-            if pow(j - w_half,2) + pow(i - h_half,2) >= rmin*rmin:
-                if pow(j - w_half,2) + pow(i - h_half,2) <= rmax*rmax:
-                    output[i, j] = fft_img[i, j]
+            if (pow(j - w_half,2) + pow(i - h_half,2) < rmin*rmin) or (pow(j - w_half,2) + pow(i - h_half,2) > rmax*rmax):
+                    output[j, i] = 0
+    return output
 
-    return(output)
+def ifft_process(img_fshift):
+    #Reversing fourier transform
+    f_ishift = np.fft.ifftshift(img_fshift)
+    ifourier = np.fft.ifft2(f_ishift)
+    output = np.abs(ifourier)
+    return output
 ##================================================
 
 #Initialization
 img, filename = get_img()
 filename_split = filename.split(".")
 
-#Calculate Fourier Transfrom of the image
-fft_img = fft_process(img)
-
-# Save the image from array
-new_filename = filename_split[0] + "-fft." + filename_split[1]
-result = img_to_file(fft_img, new_filename)
-
 #Taking the input of Rmin and Rmax
 rmin, rmax = get_r_value()
 
+#Calculate Fourier Transfrom of the image
+img_fshift = fft_process(img)
+img_log_fft = log_of_fft(img_fshift)
+
+# Save the image from array
+new_filename = filename_split[0] + "-log_fft." + filename_split[1]
+result = img_to_file(img_log_fft, new_filename)
+
 #Calculate suppression of image frequency
-suppress_freq_img = suppress_freq(result, fft_img, rmin, rmax)
+suppress_freq_img = suppress_freq(img_fshift, rmin, rmax)
+log_sup_img = log_of_fft(suppress_freq_img)
 
-#Parse the array into image
+#Save the image from array
 new_filename = filename_split[0] + "-surpressed." + filename_split[1]
-result = img_to_file(suppress_freq_img.astype(np.uint8),new_filename)
+result = img_to_file(log_sup_img,new_filename)
 
-'''
-#Parse the image into numpy tuppled array and make the value to 1 apart from 0
-a = np.asarray(new_img_2, dtype=float)
-a = np.ceil(a/a.max())
+#Calculate inverse fft to get the final result
+img_back = ifft_process(suppress_freq_img)
 
-#Multiply the Shifted fourier transform tuppled array to array a which correspond to third image 
-filtered = a*fshift
+#Save the image from array
+new_filename = filename_split[0] + "-back." + filename_split[1]
+result = img_to_file(img_back.astype(np.uint8), new_filename)
 
-#Reversing fourier transform
-f_ishift = np.fft.ifftshift(filtered)
-ifourier = np.fft.ifft2(f_ishift)
-ifourier = np.abs(ifourier)
-
-#Saving the image
-new_img_3 = ifourier.astype(np.uint8)
-result = Image.fromarray(new_img_3)
-result.save('image_3.bmp')
-print "New image_3 is available"
-'''
+#Visualization with plot
+plt.subplot(221),plt.imshow(img, cmap = 'gray')
+plt.subplot(222),plt.imshow(img_log_fft, cmap = 'gray')
+plt.subplot(223),plt.imshow(log_sup_img, cmap = 'gray')
+plt.subplot(224),plt.imshow(img_back, cmap = 'gray')
+plt.show()
